be4097f53971caac3ed13265408139233717a32a
Leonardo Miranda Gomes <contact@leonardomg.net>
2024-03-24 20:23:01 +0100

    Initial commit

---
+++ Player/Player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export_category("Settings")
+@export var speed : int = 200
+@export var animation : AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	var direction : Vector2 = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direction * speed
+
+	if direction.is_zero_approx():
+		animation.stop()
+	if direction.x == 1:
+		animation.play("Walk_Right")
+	if direction.x == -1:
+		animation.play("Walk_Left")
+	if direction.y == 1:
+		animation.play("Walk_Down")
+	if direction.y == -1:
+		animation.play("Walk_Up")
+
+	move_and_slide()
+	
